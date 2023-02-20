873eb934c01a4f6ca6ba53592a46f1402e811e85
CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
2023-02-20 00:02:43 +0000

    CHECKPOINT: First implementation of 'random' features!
    Rudimentary, of course, and not all that aesthetically pleasing... but it's a start...

--- source/HexBoard.cpp
+++ source/HexBoard.cpp
@@ -7,7 +7,7 @@ const DirectX::SimpleMath::Vector3 HexBoard::m_q = DirectX::SimpleMath::Vector3(
 
 HexBoard::HexBoard()
 {
-
+	std::srand(0);
 }
 
 
@@ -32,7 +32,6 @@ bool HexBoard::Initialize(ID3D11Device* device, int hexRadius, int cells)
 	m_horizontalField.Initialise(cells);
 	m_horizontalField.InitialiseHorizontalField(6);
 
-	std::srand(0);
 	int index = 0;
 	for (int j = -m_hexRadius; j <= m_hexRadius; j++)
 	{
@@ -41,7 +40,7 @@ bool HexBoard::Initialize(ID3D11Device* device, int hexRadius, int cells)
 			if (abs(i-j) > m_hexRadius)
 				continue;
 
-			m_hexIsolevels[index] = 0.15f+(0.15f*std::rand())/RAND_MAX;
+			m_hexIsolevels[index] = 0.15f+0.15f*std::rand()/RAND_MAX;
 
 			hexField.Initialise(&m_horizontalField);
 			hexField.DeriveHexPrism(device, m_hexIsolevels[index]);
@@ -67,11 +66,31 @@ void HexBoard::AddThorn(ID3D11Device* device, int hex)
 {
 	hex = (hex%m_hexes+m_hexes)%m_hexes;
 
+	// STEP 1: Initialise field...
 	Field hexField;
 	hexField.Initialise(&m_horizontalField);
-	hexField.IntegrateHorizontalThorn(DirectX::SimpleMath::Vector3(0.5f+0.25f*cos(2.0f*XM_PI/3.0f), 0.35f+m_hexIsolevels[hex], 0.5f-0.25f*sin(2.0f*XM_PI/3.0f)), DirectX::SimpleMath::Vector3(0.5f, 0.0f, 0.5f), XM_PIDIV2/8.0f, m_hexIsolevels[hex]);
-	hexField.IntegrateHorizontalThorn(DirectX::SimpleMath::Vector3(0.75f+0.3f*cos(-2.0f*XM_PI/3.0f), 0.4f+m_hexIsolevels[hex], 0.6f-0.3f*sin(-2.0f*XM_PI/3.0f)), DirectX::SimpleMath::Vector3(0.75f, 0.0f, 0.6f), XM_PIDIV2/8.0f, m_hexIsolevels[hex]);
-	hexField.IntegrateOrb(DirectX::SimpleMath::Vector3(0.25f, 0.45f+m_hexIsolevels[hex], 0.5f), 0.125f, m_hexIsolevels[hex]);
+
+	// STEP 2: Set thorn parameters, integrate it into the field...
+	// FIXME: A solid prototype, but highly lacking aesthetically...
+	for (int i = 0; i < 2; i++)
+	{
+		float angle = XM_PIDIV2/10.0f;
+
+		float rBaseRange = 0.5f*cos(XM_PI/6.0f);
+		float rBase = rBaseRange*sqrt(pow(((float)std::rand()/RAND_MAX), 2.0f)+pow(((float)std::rand()/RAND_MAX), 2.0f));
+		float thetaBase = XM_2PI*std::rand()/RAND_MAX;
+
+		DirectX::SimpleMath::Vector3 base = DirectX::SimpleMath::Vector3(0.5f+0.5f*rBase*cos(thetaBase), 0.0f, 0.5f+0.5f*rBase*sin(thetaBase));
+
+		float rOriginRange = cos(XM_PI/6.0f);
+		float rOrigin = rOriginRange*(0.5f+0.5f*sqrt(pow(((float)std::rand()/RAND_MAX), 2.0f)+pow(((float)std::rand()/RAND_MAX), 2.0f)));
+		float thetaOrigin = thetaBase + 2.0f*((XM_PI/3.0f)*std::rand()/RAND_MAX);
+
+		DirectX::SimpleMath::Vector3 origin = DirectX::SimpleMath::Vector3(0.5f+0.5f*rOrigin*cos(thetaOrigin), 0.4f+m_hexIsolevels[hex], 0.5f+0.5f*rBase*sin(thetaOrigin));
+
+		hexField.IntegrateHorizontalThorn(origin, base, angle, m_hexIsolevels[hex]);
+	}
+	
 	hexField.DeriveHexPrism(device, m_hexIsolevels[hex]);
 
 	m_hexModels[hex].Initialize(device, hexField.m_cells, hexField.m_field, m_hexIsolevels[hex]);
