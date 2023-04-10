4cd59c8862748d649e66756dbe078db0819dbf19
CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
2023-04-10 14:12:36 +0100

    Second sphinx tiling attempt: thinking of tiling as replacing exactly one tile with exactly 4?

--- source/LSphinxTiling.cpp
+++ source/LSphinxTiling.cpp
@@ -20,7 +20,6 @@ bool LSphinxTiling::Initialize(ID3D11Device* device, float width, int iterations
 		LModuleType productionModule = FAntiClockwiseModule;
 		productionModule.letter = "FClockwise";
 		productionModule.staticLength = FAntiClockwiseModule.staticLength/2.0f;
-		//productionModule.staticRotation = 0.0f;
 		return productionModule;
 		});
 	FAntiClockwise.productions.push_back([](LModuleType FAntiClockwiseModule) {
@@ -109,6 +108,99 @@ bool LSphinxTiling::Initialize(ID3D11Device* device, float width, int iterations
 	FAntiClockwise.weight = 1.0f;
 	AddProductionRule("FAntiClockwise", FAntiClockwise);
 
+	ProductionRuleType FClockwise;
+	FClockwise.productions.push_back([](LModuleType FClockwiseModule) {
+		LModuleType productionModule = FClockwiseModule;
+		productionModule.letter = "FAntiClockwise";
+		productionModule.staticLength = FClockwiseModule.staticLength/2.0f;
+		return productionModule;
+		});
+	FClockwise.productions.push_back([](LModuleType FClockwiseModule) {
+		LModuleType productionModule = FClockwiseModule;
+		productionModule.letter = "[";
+		return productionModule;
+		});
+	FClockwise.productions.push_back([](LModuleType FClockwiseModule) {
+		LModuleType productionModule = FClockwiseModule;
+		productionModule.letter = "~X";
+		productionModule.staticLength = FClockwiseModule.staticLength/6.0f;
+		productionModule.staticRotation = 2.0f*DirectX::XM_PI/3.0f;
+		return productionModule;
+		});
+	FClockwise.productions.push_back([](LModuleType FClockwiseModule) {
+		LModuleType productionModule = FClockwiseModule;
+		productionModule.letter = "GAntiClockwise";
+		productionModule.staticLength = FClockwiseModule.staticLength/6.0f;
+		productionModule.staticRotation = DirectX::XM_PI/3.0f;
+		return productionModule;
+		});
+	FClockwise.productions.push_back([](LModuleType FClockwiseModule) {
+		LModuleType productionModule = FClockwiseModule;
+		productionModule.letter = "X";
+		productionModule.staticLength = FClockwiseModule.staticLength/6.0f;
+		productionModule.staticRotation = -DirectX::XM_PI/3.0f;
+		return productionModule;
+		});
+	FClockwise.productions.push_back([](LModuleType FClockwiseModule) {
+		LModuleType productionModule = FClockwiseModule;
+		productionModule.letter = "HAntiClockwise";
+		productionModule.staticLength = FClockwiseModule.staticLength/3.0f;
+		productionModule.staticRotation = 2.0f*DirectX::XM_PI/3.0f;
+		return productionModule;
+		});
+	FClockwise.productions.push_back([](LModuleType FClockwiseModule) {
+		LModuleType productionModule = FClockwiseModule;
+		productionModule.letter = "]";
+		return productionModule;
+		});
+	FClockwise.productions.push_back([](LModuleType FClockwiseModule) {
+		LModuleType productionModule = FClockwiseModule;
+		productionModule.letter = "FAntiClockwise";
+		productionModule.staticLength = FClockwiseModule.staticLength/2.0f;
+		productionModule.staticRotation = 0.0f;
+		return productionModule;
+		});
+	FClockwise.productions.push_back([](LModuleType FClockwiseModule) {
+		LModuleType productionModule = FClockwiseModule;
+		productionModule.letter = "[";
+		return productionModule;
+		});
+	FClockwise.productions.push_back([](LModuleType FClockwiseModule) {
+		LModuleType productionModule = FClockwiseModule;
+		productionModule.letter = "~X";
+		productionModule.staticLength = FClockwiseModule.staticLength/6.0f;
+		productionModule.staticRotation = 2.0f*DirectX::XM_PI/3.0f;
+		return productionModule;
+		});
+	FClockwise.productions.push_back([](LModuleType FClockwiseModule) {
+		LModuleType productionModule = FClockwiseModule;
+		productionModule.letter = "GAntiClockwise";
+		productionModule.staticLength = FClockwiseModule.staticLength/6.0f;
+		productionModule.staticRotation = DirectX::XM_PI/3.0f;
+		return productionModule;
+		});
+	FClockwise.productions.push_back([](LModuleType FClockwiseModule) {
+		LModuleType productionModule = FClockwiseModule;
+		productionModule.letter = "X";
+		productionModule.staticLength = FClockwiseModule.staticLength/6.0f;
+		productionModule.staticRotation = -DirectX::XM_PI/3.0f;
+		return productionModule;
+		});
+	FClockwise.productions.push_back([](LModuleType FClockwiseModule) {
+		LModuleType productionModule = FClockwiseModule;
+		productionModule.letter = "HAntiClockwise";
+		productionModule.staticLength = FClockwiseModule.staticLength/3.0f;
+		productionModule.staticRotation = 2.0f*DirectX::XM_PI/3.0f;
+		return productionModule;
+		});
+	FClockwise.productions.push_back([](LModuleType FClockwiseModule) {
+		LModuleType productionModule = FClockwiseModule;
+		productionModule.letter = "]";
+		return productionModule;
+		});
+	FClockwise.weight = 1.0f;
+	AddProductionRule("FClockwise", FClockwise);
+
 	ProductionRuleType GAntiClockwise;
 	GAntiClockwise.productions.push_back([](LModuleType GAntiClockwiseModule) {
 		LModuleType productionModule = GAntiClockwiseModule;
@@ -134,21 +226,51 @@ bool LSphinxTiling::Initialize(ID3D11Device* device, float width, int iterations
 	GAntiClockwise.weight = 1.0f;
 	AddProductionRule("GAntiClockwise", GAntiClockwise);
 
+	ProductionRuleType GClockwise;
+	GClockwise.productions.push_back([](LModuleType GClockwiseModule) {
+		LModuleType productionModule = GClockwiseModule;
+		productionModule.letter = "X";	
+		productionModule.staticLength = GClockwiseModule.staticLength/2.0f;
+		productionModule.staticRotation = GClockwiseModule.staticRotation-DirectX::XM_PI; // NB: Go back on self, then forward!
+		return productionModule;
+		});
+	GClockwise.productions.push_back([](LModuleType GClockwiseModule) {
+		LModuleType productionModule = GClockwiseModule;
+		productionModule.letter = "FAntiClockwise";
+		productionModule.staticLength = 3.0f*GClockwiseModule.staticLength/2.0f;
+		productionModule.staticRotation = DirectX::XM_PI;
+		return productionModule;
+		});
+
+	GClockwise.weight = 1.0f;
+	AddProductionRule("GClockwise", GClockwise);
+
 	ProductionRuleType HAntiClockwise;
 	HAntiClockwise.productions.push_back([](LModuleType HAntiClockwiseModule) {
 		LModuleType productionModule = HAntiClockwiseModule;
-		productionModule.letter = "FClockwise";
+		productionModule.letter = "FAntiClockwise";
 		productionModule.staticLength = 3.0f*HAntiClockwiseModule.staticLength/4.0f;
-		return productionModule;
-		});
-	HAntiClockwise.productions.push_back([](LModuleType HAntiClockwiseModule) {
-		LModuleType productionModule = HAntiClockwiseModule;
-		productionModule.letter = "~X";
-		productionModule.staticRotation = 0.0f;
 		return productionModule;
 		});
 	HAntiClockwise.weight = 1.0f;
 	AddProductionRule("HAntiClockwise", HAntiClockwise);
+
+	ProductionRuleType HClockwise;
+	HClockwise.productions.push_back([](LModuleType HClockwiseModule) {
+		LModuleType productionModule = HClockwiseModule;
+		productionModule.letter = "FClockwise";
+		productionModule.staticLength = 3.0f*HClockwiseModule.staticLength/4.0f;
+		return productionModule;
+		});
+	HClockwise.productions.push_back([](LModuleType HClockwiseModule) {
+		LModuleType productionModule = HClockwiseModule;
+		productionModule.letter = "X";
+		productionModule.staticLength = HClockwiseModule.staticLength/4.0f;
+		productionModule.staticRotation = 0.0f;
+		return productionModule;
+		});
+	HClockwise.weight = 1.0f;
+	AddProductionRule("HClockwise", HClockwise);
 
 	ProductionRuleType X;
 	X.weight = 1.0f;
@@ -200,7 +322,7 @@ bool LSphinxTiling::Initialize(ID3D11Device* device, float width, int iterations
 	HAntiClockwiseModule.staticLength = 2.0f;
 	HAntiClockwiseModule.staticRotation = 2.0f*DirectX::XM_PI/3.0f;
 
-	std::vector<LModuleType> axiom = std::vector<LModuleType>{ FAntiClockwiseModule, XModule1, GAntiClockwiseModule, XModule2, HAntiClockwiseModule };
+	std::vector<LModuleType> axiom = std::vector<LModuleType>{ FAntiClockwiseModule , XModule1, GAntiClockwiseModule, XModule2, HAntiClockwiseModule };
 
 	// STEP 3: Initialise...
 	return __super::Initialize(device, axiom, iterations, seed, 0.0f); // NB: Oriented to best 'tend towards a limit'...
